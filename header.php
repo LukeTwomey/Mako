c6ab4da366e68c048d2730b6dd0b8490d63ba8eb
Luke Twomey <luke@luketwomey.com>
2016-06-24 16:13:44 +0100

    Add header logo styling.

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <header>
     <img class="menu" src="/images/icons/menu.png"/>
     <div class="logo-nav-bar">
-        <!-- <a href="index.php">
-            <img class="logo" src="/images/logo.png"/>
-        </a> -->
+        <a href="index.php" class="logo">
+            <h1>Mako</h1>
+        </a>
         <nav>
             <?php include("nav.php"); ?>
         </nav>
