776632362b0551f4be16aa84f87f2a27ac27f8f5
Luke Twomey <luke@luketwomey.com>
2016-06-25 18:57:32 +0100

    Add additional image block to homepage.

--- index.php
+++ index.php
@@ -29,8 +29,26 @@
                         </div>
                     </div>
 
-                    <h2>We give your business an identity</h2>
-                    <p>One morning, when Gregor Samsa woke from <strong>troubled</strong> dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.</p>
+                    <h2>We help give your business a unique identity.</h2>
+
+                    <div class="grid">
+                        <div class="block-sizer"></div>
+                        <div class="gutter-sizer"></div>
+                        <div class="block">
+                            <img src="images/watch-shades-pouch.jpg"/>
+                        </div>
+                        <div class="block">
+                            <img src="images/blue-hair.jpg"/>
+                        </div>
+                        
+                       
+                        <div class="block"> 
+                            <img src="images/geometric-waves.jpg"/>
+                        </div>
+                         <div class="block">
+                            <img src="images/macaroons.jpg"/>
+                        </div>
+                    </div>
 
                 </div>
 
