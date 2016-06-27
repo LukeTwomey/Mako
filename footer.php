35b4e9f45cfba2bcffcc84e5f70289044d13fcdf
Luke Twomey <luke@luketwomey.com>
2016-06-27 14:49:35 +0100

    Change font for logo in footer.

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
 	<ul>
 		<li><a href="https://www.facebook.com/LukeDTwomey"><h3>Facebook</h3></a></li>
 		<li><a href="https://twitter.com/LukeDTwomey"><h3>Twitter</h3></a><br/></li>
-		<li><h3>&copy; Mako 2016</h3></li>
+		<li><h3 class="copyright">&copy; <strong>Mako</strong> 2016</h3></li>
 	</ul>
 </footer>
