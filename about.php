b1c57db9d4caf4b3a8a3e484eee7e742c7826662
Luke Twomey <luke@luketwomey.com>
2016-07-13 20:59:55 +0100

    Create template about page.

---
+++ about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <?php include("head.php"); ?>
+    </head>
+
+    <body>
+        <div id="fouc">
+            <div class="site-wrap">
+                <?php include("header.php"); ?>
+
+                <div class="page-content about"> 
+                    <h1>About</h1>
+                </div>
+
+                <?php include("footer.php"); ?>
+            </div>
+        </div>
+
+        <?php include("back-to-top.php"); ?>
+        <?php include("javascript-files.php"); ?>
+    </body>
+</html>
