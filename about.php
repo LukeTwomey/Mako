4be30278f95dd17ccd871f9921470e2e795df123
Luke Twomey <luke@luketwomey.com>
2016-07-19 20:50:33 +0100

    Style about page for desktop layout.

--- about.php
+++ about.php
@@ -12,11 +12,13 @@
 
                 <div class="page-content about"> 
                     <img src="images/goatee-m.jpg"/>
-                    <h1>About</h1>
-                    <h2>We are Mako.</h2>
-                    <p>Mako are a small team of designers and developers based in London.</p>
-                    <p>We specialise in conveying our client's messages through the power of branding and design.</p>
-                    <p>Large corporations. Local high street businesses. Our services can be employed across a wide range of projects.</p>
+                    <div class="our-info">
+                        <h1>About</h1>
+                        <h2>We are Mako.</h2>
+                        <p>Mako are a small team of designers and developers based in London.</p>
+                        <p>We specialise in conveying our client's messages through the power of branding and design.</p>
+                        <p>Large corporations. Local high street businesses. Our services can be employed across a wide range of projects.</p>
+                    </div>
                 </div>
 
                 <?php include("footer.php"); ?>
