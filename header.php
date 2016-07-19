65d1ab25f75d8e0e157a1db5d18060af05e2e8e2
Luke Twomey <luke@luketwomey.com>
2016-07-19 21:01:16 +0100

    Set maximum width for the site.

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <header>
-    <img class="menu" src="/images/icons/menu.png"/>
-    <img class="close" src="/images/icons/close.png"/>
-    <div class="logo-nav-bar">
-        <a href="index.php" class="logo">
-            <h1>Mako</h1>
-        </a>
-        <nav>
-            <?php include("nav.php"); ?>
-        </nav>
-    </div>
+        <img class="menu" src="/images/icons/menu.png"/>
+        <img class="close" src="/images/icons/close.png"/>
+        <div class="logo-nav-bar">
+            <a href="index.php" class="logo">
+                <h1>Mako</h1>
+            </a>
+            <nav>
+                <?php include("nav.php"); ?>
+            </nav>
+        </div>
 </header>
