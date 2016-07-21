03ad3ebd2d750d189633a931771fe25af46675d6
Luke Twomey <luke@luketwomey.com>
2016-07-21 19:43:27 +0100

    Add project titles to homepage tiles. Adjust positioning.

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
                             <a href="binksy.php">
                                 <img src="images/pastel-blocks.jpg"/>
                                 <div class="hover-reveal">
+                                    <h2>Pastel Addicts</h2>
                                 </div>
                             </a>
                         </div>
@@ -33,6 +34,7 @@
                             <a href="binksy.php">
                                 <img src="images/retro-blue.jpg"/>
                                 <div class="hover-reveal">
+                                    <h2>Safe Harbour</h2>
                                 </div>
                             </a>
                         </div>
@@ -40,6 +42,7 @@
                             <a href="binksy.php">
                                 <img src="images/shoes.jpg"/>
                                 <div class="hover-reveal">
+                                    <h2>Crazy Feet</h2>
                                 </div>
                             </a>
                         </div>
@@ -54,6 +57,7 @@
                             <a href="binksy.php">
                                 <img src="images/watch-shades-pouch.jpg"/>
                                 <div class="hover-reveal">
+                                    <h2>Summer Haze</h2>
                                 </div>
                             </a>
                         </div>
@@ -61,6 +65,7 @@
                             <a href="binksy.php">
                                 <img src="images/blue-hair.jpg"/>
                                 <div class="hover-reveal">
+                                    <h2>Style Boutique</h2>
                                 </div>
                             </a>
                         </div>
@@ -68,6 +73,7 @@
                             <a href="binksy.php">
                                 <img src="images/geometric-waves.jpg"/>
                                 <div class="hover-reveal">
+                                    <h2>Randall Gallery</h2>
                                 </div>
                             </a>
                         </div>
@@ -75,6 +81,7 @@
                             <a href="binksy.php">
                                 <img src="images/macaroons.jpg"/>
                                 <div class="hover-reveal">
+                                    <h2>Patisserie We Love</h2>
                                 </div>
                             </a>
                         </div>
