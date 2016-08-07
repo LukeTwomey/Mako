286b875b238ca4ef7ff0284e55e06ad3ec514ce0
Luke Twomey <luke@luketwomey.com>
2016-08-07 11:46:05 +0100

    Create Pastel Addicts project page.

---
+++ pastel-addicts.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <?php include("head.php"); ?>
+    </head>
+
+    <body>
+        <div id="fouc">
+            <div class="site-wrap">
+                <?php include("header.php"); ?>
+
+                <div class="page-content binksy project"> 
+                    <img src="images/graffiti-wall.jpg"/>
+
+                    <h1>Binksy</h1>
+                    <p>Binksy is a London based street artist with a unique style and exceptional talent.</p>
+                    <p>We worked closely with Binksy to market his skills and propel him into the mainstream.</p>
+
+                    <img src="images/cigarette-s.jpg" srcset="images/cigarette-m.jpg 800w, images/cigarette-l.jpg 1200w, images/cigarette-xl.jpg 1600w" alt="Smoking cigarette graffiti"/>
+
+                    <img class="portrait left" src="images/glasses-s.jpg" srcset="images/glasses-m.jpg 800w, images/glasses-l.jpg 1200w, images/glasses-xl.jpg 1600w" alt="Man wearing glasses graffiti"/>
+
+                    <img class="portrait right" src="images/joker-s.jpg" srcset="images/joker-m.jpg 800w, images/joker-l.jpg 1200w, images/joker-xl.jpg 1600w" alt="The Joker graffiti"/>
+
+                    <img src="images/graffiti-girl-s.jpg" srcset="images/graffiti-girl-m.jpg 800w, images/graffiti-girl-l.jpg 1200w, images/graffiti-girl-xl.jpg 1600w" alt="Graffiti girl crouching"/>
+
+                </div>
+
+                <?php include("footer.php"); ?>
+            </div>
+        </div>
+
+        <?php include("back-to-top.php"); ?>
+        <?php include("javascript-files.php"); ?>
+    </body>
+</html>
