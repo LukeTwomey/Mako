bb063813e2a7735abe7bd0291bc646cde862fabc
Luke Twomey <luke@luketwomey.com>
2016-08-09 21:46:43 +0100

    Create base Safe Harbour project page.

---
+++ safe-harbour.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <?php include("head.php"); ?>
+    </head>
+
+    <body>
+        <div id="fouc">
+            <div class="site-wrap">
+                <?php include("header.php"); ?>
+
+                <div class="page-content safe-harbour project"> 
+                    <img src="images/pastel-blocks-main.jpg"/>
+
+                    <h1>Safe Harbour</h1>
+                    <p>Safe Harbour .</p>
+
+                    <img src="images/brushes-s.jpg" srcset="images/brushes-m.jpg 800w, images/brushes-l.jpg 1200w, images/brushes-xl.jpg 1500w" alt="Paint brushes"/>
+
+                    <img src="images/gallery-s.jpg" srcset="images/gallery-m.jpg 800w, images/gallery-l.jpg 1200w, images/gallery-xl.jpg 1600w" alt="Gallery studio"/>
+
+                    <img src="images/easel-s.jpg" srcset="images/easel-m.jpg 800w, images/easel-l.jpg 1200w, images/easel-xl.jpg 1600w" alt="Easel"/>                    
+
+                </div>
+
+                <?php include("footer.php"); ?>
+            </div>
+        </div>
+
+        <?php include("back-to-top.php"); ?>
+        <?php include("javascript-files.php"); ?>
+    </body>
+</html>
