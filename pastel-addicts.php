cf57e0e2fbf6021e1751aa99650973f1d3e6b630
Luke Twomey <luke@luketwomey.com>
2016-08-14 16:51:47 +0100

    Replace images on Pastel Addicts page with final versions.

--- pastel-addicts.php
+++ pastel-addicts.php
@@ -11,12 +11,12 @@
                 <?php include("header.php"); ?>
 
                 <div class="page-content binksy project"> 
-                    <img src="images/pastel-blocks-main.jpg"/>
+                    <img src="images/pastel-addicts-s.jpg" srcset="images/pastel-addicts-m.jpg 800w, images/pastel-addicts-l.jpg 1200w, images/pastel-addicts-xl.jpg 1500w" alt="Pastel coloured squares"/>
 
                     <h1>Pastel Addicts</h1>
                     <p>Pastel Addicts are a unique, quirky art supply store in central London. The twist - they actually own a gallery where the supplies are sold. This beautiful building is minimal, and art focused. Their branding needed to match.</p>
 
-                    <img src="images/brushes-s.jpg" srcset="images/brushes-m.jpg 800w, images/brushes-l.jpg 1200w, images/brushes-xl.jpg 1500w" alt="Paint brushes"/>
+                    <img src="images/paintbrushes-s.jpg" srcset="images/paintbrushes-m.jpg 800w, images/paintbrushes-l.jpg 1200w, images/paintbrushes-xl.jpg 1500w" alt="Paint brushes"/>
 
                     <img src="images/gallery-s.jpg" srcset="images/gallery-m.jpg 800w, images/gallery-l.jpg 1200w, images/gallery-xl.jpg 1600w" alt="Gallery studio"/>
 
