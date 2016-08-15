ef083b402f48959d6191e1e00a28d3de8f93e042
Luke Twomey <luke@luketwomey.com>
2016-08-15 21:00:22 +0100

    Add final images to Patisserie We Love project page.

--- patisserie-we-love.php
+++ patisserie-we-love.php
@@ -11,18 +11,16 @@
                 <?php include("header.php"); ?>
 
                 <div class="page-content patisserie-we-love project"> 
-                    <img src="images/pastel-addicts-s.jpg" srcset="images/pastel-addicts-m.jpg 800w, images/pastel-addicts-l.jpg 1200w, images/pastel-addicts-xl.jpg 1500w" alt="Pastel coloured squares"/>
+                    <img src="images/patisserie-we-love-s.jpg" srcset="images/patisserie-we-love-m.jpg 800w, images/patisserie-we-love-l.jpg 1200w, images/patisserie-we-love-xl.jpg 1500w" alt="Macaroons"/>
 
                     <h1>Patisserie We Love</h1>
                     <p>We love patisserie. This was a match made in heaven. Patisserie We Love produce the nicest food you will ever have the fortune of placing in your mouth. With an already stellar reputation from a loyal customer base (many of which existed within our team) we worked with Patisserie We Love to get them more exposure.</p>
 
                     <p>Their brand is now widely recognised online, and customers can now benefit from their fresh delivery service. Imagine - fresh, tasty patisserie delivered directly to your door. What a world we live in.</p>
 
-                    <img src="images/paintbrushes-s.jpg" srcset="images/paintbrushes-m.jpg 800w, images/paintbrushes-l.jpg 1200w, images/paintbrushes-xl.jpg 1500w" alt="Paint brushes"/>
+                    <img src="images/patisserie-we-love-2-s.jpg" srcset="images/patisserie-we-love-2-m.jpg 800w, images/patisserie-we-love-2-l.jpg 1200w, images/patisserie-we-love-2-xl.jpg 1500w" alt="Macaroons"/>
 
-                    <img src="images/gallery-s.jpg" srcset="images/gallery-m.jpg 800w, images/gallery-l.jpg 1200w, images/gallery-xl.jpg 1600w" alt="Gallery studio"/>
-
-                    <img src="images/easel-s.jpg" srcset="images/easel-m.jpg 800w, images/easel-l.jpg 1200w, images/easel-xl.jpg 1600w" alt="Easel"/>                    
+                    <img src="images/patisserie-we-love-3-s.jpg" srcset="images/patisserie-we-love-3-m.jpg 800w, images/patisserie-we-love-3-l.jpg 1200w, images/patisserie-we-love-3-xl.jpg 1500w" alt="Macaroons"/>                  
 
                 </div>
 
