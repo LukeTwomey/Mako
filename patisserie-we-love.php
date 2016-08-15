ba66a476d881090925c77b04348ad39f6bec71e0
Luke Twomey <luke@luketwomey.com>
2016-08-15 19:52:03 +0100

    Create Patisserie We Love project page and add text.

---
+++ patisserie-we-love.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <?php include("head.php"); ?>
+    </head>
+
+    <body>
+        <div id="fouc">
+            <div class="site-wrap">
+                <?php include("header.php"); ?>
+
+                <div class="page-content patisserie-we-love project"> 
+                    <img src="images/pastel-addicts-s.jpg" srcset="images/pastel-addicts-m.jpg 800w, images/pastel-addicts-l.jpg 1200w, images/pastel-addicts-xl.jpg 1500w" alt="Pastel coloured squares"/>
+
+                    <h1>Patisserie We Love</h1>
+                    <p>We love patisserie. This was a match made in heaven. Patisserie We Love produce the nicest food you will ever have the fortune of placing in your mouth. With an already stellar reputation from a loyal customer base (many of which existed within our team) we worked with Patisserie We Love to get them more exposure.</p>
+
+                    <p>Their brand is now widely recognised online, and customers can now benefit from their fresh delivery service. Imagine - fresh, tasty patisserie delivered directly to your door. What a world we live in.</p>
+
+                    <img src="images/paintbrushes-s.jpg" srcset="images/paintbrushes-m.jpg 800w, images/paintbrushes-l.jpg 1200w, images/paintbrushes-xl.jpg 1500w" alt="Paint brushes"/>
+
+                    <img src="images/gallery-s.jpg" srcset="images/gallery-m.jpg 800w, images/gallery-l.jpg 1200w, images/gallery-xl.jpg 1600w" alt="Gallery studio"/>
+
+                    <img src="images/easel-s.jpg" srcset="images/easel-m.jpg 800w, images/easel-l.jpg 1200w, images/easel-xl.jpg 1600w" alt="Easel"/>                    
+
+                </div>
+
+                <?php include("footer.php"); ?>
+            </div>
+        </div>
+
+        <?php include("back-to-top.php"); ?>
+        <?php include("javascript-files.php"); ?>
+    </body>
+</html>
