aff863e15ba071150f8329bb7ee745465407cd42
Luke Twomey <luke@luketwomey.com>
2016-08-15 19:49:31 +0100

    Create Style Boutique project page and add text.

---
+++ style-boutique.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <?php include("head.php"); ?>
+    </head>
+
+    <body>
+        <div id="fouc">
+            <div class="site-wrap">
+                <?php include("header.php"); ?>
+
+                <div class="page-content style-boutique project"> 
+                    <img src="images/pastel-addicts-s.jpg" srcset="images/pastel-addicts-m.jpg 800w, images/pastel-addicts-l.jpg 1200w, images/pastel-addicts-xl.jpg 1500w" alt="Pastel coloured squares"/>
+
+                    <h1>Style Boutique</h1>
+                    <p>With celebrities from Kanye East to Taylor Swoft clamouring to get through the door, Style Boutique is the hottest in the world right now. With their fingers on the pulse of the cutting edge fashion and beauty scene, they make sure you look on point at all times.</p>
+                    <p>We brand them the best way we know how; by showcasing their amazing, stylish creations to the world.</p>
+
+                    <img src="images/paintbrushes-s.jpg" srcset="images/paintbrushes-m.jpg 800w, images/paintbrushes-l.jpg 1200w, images/paintbrushes-xl.jpg 1500w" alt="Paint brushes"/>
+
+                    <img src="images/gallery-s.jpg" srcset="images/gallery-m.jpg 800w, images/gallery-l.jpg 1200w, images/gallery-xl.jpg 1600w" alt="Gallery studio"/>
+
+                    <img src="images/easel-s.jpg" srcset="images/easel-m.jpg 800w, images/easel-l.jpg 1200w, images/easel-xl.jpg 1600w" alt="Easel"/>                    
+
+                </div>
+
+                <?php include("footer.php"); ?>
+            </div>
+        </div>
+
+        <?php include("back-to-top.php"); ?>
+        <?php include("javascript-files.php"); ?>
+    </body>
+</html>
