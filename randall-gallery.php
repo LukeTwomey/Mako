6edf6f1847bf5b26ed02a69bec4d21ecc736bd23
Luke Twomey <luke@luketwomey.com>
2016-08-15 19:50:28 +0100

    Create Randall Gallery project page and add text.

---
+++ randall-gallery.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <?php include("head.php"); ?>
+    </head>
+
+    <body>
+        <div id="fouc">
+            <div class="site-wrap">
+                <?php include("header.php"); ?>
+
+                <div class="page-content randall-gallery project"> 
+                    <img src="images/pastel-addicts-s.jpg" srcset="images/pastel-addicts-m.jpg 800w, images/pastel-addicts-l.jpg 1200w, images/pastel-addicts-xl.jpg 1500w" alt="Pastel coloured squares"/>
+
+                    <h1>Randall Gallery</h1>
+                    <p>With all the popularity of the Tate Modern, in a smaller, hipper package, Randall Gallery epitomises all that is great about modern art. As a trendier alternative to the more widely known galleries in London, our job was to elevate their public image to start to achieve more widespread recognition. With our help, they are becoming bigger than ever.</p>
+
+                    <img src="images/paintbrushes-s.jpg" srcset="images/paintbrushes-m.jpg 800w, images/paintbrushes-l.jpg 1200w, images/paintbrushes-xl.jpg 1500w" alt="Paint brushes"/>
+
+                    <img src="images/gallery-s.jpg" srcset="images/gallery-m.jpg 800w, images/gallery-l.jpg 1200w, images/gallery-xl.jpg 1600w" alt="Gallery studio"/>
+
+                    <img src="images/easel-s.jpg" srcset="images/easel-m.jpg 800w, images/easel-l.jpg 1200w, images/easel-xl.jpg 1600w" alt="Easel"/>                    
+
+                </div>
+
+                <?php include("footer.php"); ?>
+            </div>
+        </div>
+
+        <?php include("back-to-top.php"); ?>
+        <?php include("javascript-files.php"); ?>
+    </body>
+</html>
