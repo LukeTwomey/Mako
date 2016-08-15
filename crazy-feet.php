5cb4a60290a8e0f96324a3ee6fe1e13c0e1b55fd
Luke Twomey <luke@luketwomey.com>
2016-08-15 20:12:12 +0100

    Add final images for Crazy Feet project page.

--- crazy-feet.php
+++ crazy-feet.php
@@ -11,17 +11,17 @@
                 <?php include("header.php"); ?>
 
                 <div class="page-content crazy-feet project"> 
-                    <img src="images/pastel-addicts-s.jpg" srcset="images/pastel-addicts-m.jpg 800w, images/pastel-addicts-l.jpg 1200w, images/pastel-addicts-xl.jpg 1500w" alt="Pastel coloured squares"/>
+                    <img src="images/crazy-feet-s.jpg" srcset="images/crazy-feet-m.jpg 800w, images/crazy-feet-l.jpg 1200w, images/crazy-feet-xl.jpg 1600w" alt="Shoes with striking patterns."/>
 
                     <h1>Crazy Feet</h1>
                     <p>Crazy Feet are a well established footwear brand catering to those among us who detest sane shoes. The designs completely earn their crazy title, and are perfectly suited to the pared back, classy branding we know and love.</p>
                     <p>The contrast between the fantastically funky footwear and the block colours surrounding them shows off the kicks in all their glory.</p>
 
-                    <img src="images/paintbrushes-s.jpg" srcset="images/paintbrushes-m.jpg 800w, images/paintbrushes-l.jpg 1200w, images/paintbrushes-xl.jpg 1500w" alt="Paint brushes"/>
+                    <img src="images/crazy-feet-4-s.jpg" srcset="images/crazy-feet-4-m.jpg 800w, images/crazy-feet-4-l.jpg 1200w, images/crazy-feet-4-xl.jpg 1600w" alt="Shoes with striking patterns."/>
 
-                    <img src="images/gallery-s.jpg" srcset="images/gallery-m.jpg 800w, images/gallery-l.jpg 1200w, images/gallery-xl.jpg 1600w" alt="Gallery studio"/>
+                    <img src="images/crazy-feet-3-s.jpg" srcset="images/crazy-feet-3-m.jpg 800w, images/crazy-feet-3-l.jpg 1200w, images/crazy-feet-3-xl.jpg 1600w" alt="Shoes with striking patterns."/>
 
-                    <img src="images/easel-s.jpg" srcset="images/easel-m.jpg 800w, images/easel-l.jpg 1200w, images/easel-xl.jpg 1600w" alt="Easel"/>                    
+                    <img src="images/crazy-feet-2-s.jpg" srcset="images/crazy-feet-2-m.jpg 800w, images/crazy-feet-2-l.jpg 1200w, images/crazy-feet-2-xl.jpg 1600w" alt="Shoes with striking patterns."/>                 
 
                 </div>
 
