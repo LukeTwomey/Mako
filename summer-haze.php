fb7e37ef8dfd7b1e9b4b7be94e6f9b1839c3eeb6
Luke Twomey <luke@luketwomey.com>
2016-08-15 19:47:23 +0100

    Create Summer Haze project page and add text.

---
+++ summer-haze.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <?php include("head.php"); ?>
+    </head>
+
+    <body>
+        <div id="fouc">
+            <div class="site-wrap">
+                <?php include("header.php"); ?>
+
+                <div class="page-content summer-haze project"> 
+                    <img src="images/pastel-addicts-s.jpg" srcset="images/pastel-addicts-m.jpg 800w, images/pastel-addicts-l.jpg 1200w, images/pastel-addicts-xl.jpg 1500w" alt="Pastel coloured squares"/>
+
+                    <h1>CSummer Haze</h1>
+                    <p>Summer Haze know how to make you look hot in the sun. As part of the larger Force 9 clothing brand, their summer spin-off label is aimed squarely at the scorching end of the market. Need to look good at that beach BBQ? They've got you covered.</p>
+
+                    <img src="images/paintbrushes-s.jpg" srcset="images/paintbrushes-m.jpg 800w, images/paintbrushes-l.jpg 1200w, images/paintbrushes-xl.jpg 1500w" alt="Paint brushes"/>
+
+                    <img src="images/gallery-s.jpg" srcset="images/gallery-m.jpg 800w, images/gallery-l.jpg 1200w, images/gallery-xl.jpg 1600w" alt="Gallery studio"/>
+
+                    <img src="images/easel-s.jpg" srcset="images/easel-m.jpg 800w, images/easel-l.jpg 1200w, images/easel-xl.jpg 1600w" alt="Easel"/>                    
+
+                </div>
+
+                <?php include("footer.php"); ?>
+            </div>
+        </div>
+
+        <?php include("back-to-top.php"); ?>
+        <?php include("javascript-files.php"); ?>
+    </body>
+</html>
