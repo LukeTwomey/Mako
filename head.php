808b76d08fbc0a772545b57d4ba32470bd7a1f8b
Luke Twomey <luke@luketwomey.com>
2016-08-16 20:56:05 +0100

    Create favicons.

--- head.php
+++ head.php
@@ -7,16 +7,16 @@
 </script>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0" />
-<link rel="icon" type="image/png" href="/images/icons/favicon-16x16.png" sizes="16x16" />
-<link rel="icon" type="image/png" href="/images/icons/favicon-32x32.png" sizes="32x32" />
-<link rel="icon" type="image/x-icon" href="/images/icons/favicon.ico" />
-<link rel="shortcut icon" type="image/x-icon" href="/images/icons/favicon.ico" />
-<link rel="stylesheet" href="/stylesheets/css-reset-min.css" type="text/css" />
+<link rel="icon" type="image/png" href="images/icons/favicon-16x16.png" sizes="16x16" />
+<link rel="icon" type="image/png" href="images/icons/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/x-icon" href="images/icons/favicon.ico" />
+<link rel="shortcut icon" type="image/x-icon" href="images/icons/favicon.ico" />
+<link rel="stylesheet" href="stylesheets/css-reset-min.css" type="text/css" />
 
-<link rel="stylesheet/less" type="text/css" href="/stylesheets/style.less" />
+<link rel="stylesheet/less" type="text/css" href="stylesheets/style.less" />
 <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.0/less.min.js"></script>
 
 <!--<link rel="stylesheet" href="/stylesheets/style.css" type="text/css" />-->
-<link rel="stylesheet" href="/stylesheets/jquery.fancybox.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="stylesheets/jquery.fancybox.css" type="text/css" media="screen" />
 
 <link href='https://fonts.googleapis.com/css?family=Khula:400,700|Syncopate:400,700' rel='stylesheet' type='text/css'>
